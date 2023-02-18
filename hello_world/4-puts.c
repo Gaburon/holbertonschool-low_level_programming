6eb6fc8846a2fd6f421739b6c9eb812260923811
Gaburon <japo000@hotmail.com>
2023-02-17 16:02:36 -0800

    BETYY

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+
 /**
  * main - prints "with proper grammar
  *
  * Return: always 0
  */
+
 int main(void)
+
 {
         puts("\"Programming is like building a multilingual puzzle");
         return (0);
 }
-
