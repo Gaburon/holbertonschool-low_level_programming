fe517e5d814df9b5589d3b56bd160cee84df8845
Gaburon <Japo000@hotmail.com>
2023-02-23 07:48:11 -0800

    10-add

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *Add - adds two values
+ *@n1: The first num to be computed
+ *@n2: The second number to be computed
+ *Return: Integer value
+ */
+
+int add(int n1, int n2)
+
+{
+
+  return (n1 + n2);
+
+}
