86e41ac2d7e0bacc727d43ca2be8141bbcd53a50
Gaburon <japo000@hotmail.com>
2023-03-02 20:08:55 -0800

    6-cap_string.c

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @str: string to capitalize
+ *
+ * Return: capitalized string
+ */
+char *cap_string(char *str)
+{
+	int i;
+
+	if (str[0] >= 'a' && str[0] <= 'z')
+		str[0] -= 32;
+
+	for (i = 1; str[i] != '\0'; i++)
+	{
+		if ((str[i] >= 'a' && str[i] <= 'z') &&
+		    (str[i - 1] == ' ' || str[i - 1] == '\t' ||
+		     str[i - 1] == '\n' || str[i - 1] == ',' ||
+		     str[i - 1] == ';' || str[i - 1] == '.' ||
+		     str[i - 1] == '!' || str[i - 1] == '?' ||
+		     str[i - 1] == '"' || str[i - 1] == '(' ||
+		     str[i - 1] == ')' || str[i - 1] == '{' ||
+		     str[i - 1] == '}'))
+			str[i] -= 32;
+	}
+
+	return (str);
+}
