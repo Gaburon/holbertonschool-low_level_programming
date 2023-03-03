ebae26e164dd541d2ca7dd47a36dae7f22351a3c
Gaburon <japo000@hotmail.com>
2023-03-02 19:53:21 -0800

    4-rev_array.c

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * reverse_array - invierte mi array
+ * @a: array
+ * @n: numero de elementos
+ *
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < n--; i++)
+	{
+		j = a[i];
+		a[i] = a[n];
+		a[n] = j;
+	}
+}
