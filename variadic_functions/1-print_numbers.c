d767c1cdf8996bad3e7506829fc9346e780e3a72
Gaburon <japo000@hotmail.com>
2023-03-24 20:17:02 -0700

    Betty fix

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -12,7 +12,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
 	va_list list;
-	va_start (list, n);
+	
+	va_start(list, n);
 	for (i = 0; i < n; i++)
 	{
 		if (i > 0 && separator != NULL)
