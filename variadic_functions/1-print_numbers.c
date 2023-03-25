53a0bc8f36b14c27ef9c45accf04778c63be5d99
Gaburon <japo000@hotmail.com>
2023-03-24 19:01:14 -0700

    1-print_numbers.c

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+
+
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list list;
+
+	if (separator == NULL)
+		separator = '\0';
+	va_start(list, n);
+	if (n != 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			printf("%d", va_arg(list, int));
+		}
+
+		va_end(list);
+		prinft("\n");
+	}
