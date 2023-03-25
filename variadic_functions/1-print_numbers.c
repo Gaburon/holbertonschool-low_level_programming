4d08d41438101903096e71a0bd0bd10384d65750
Gaburon <japo000@hotmail.com>
2023-03-24 19:59:33 -0700

    1-print_numbers.c

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -1,10 +1,11 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
 #include <stdio.h>
-
-
-
-
+/**
+ * print_numbers - prints numbers
+ * @separator: seperator char
+ * @n: integers to print
+ */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
