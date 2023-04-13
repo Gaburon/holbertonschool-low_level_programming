5229543368074e52841e4cfed234a0bcbe363f40
Gaburon <japo000@hotmail.com>
2023-04-13 10:51:49 -0700

    3-cp.c

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,54 +1,96 @@
 #include "main.h"
-/**
- * main - program to copy
- * @ac: argument count
- * @av: array of arguments
- * Return: a value
- */
-int main(int ac, char **av)
-{
-	int fdFrum, fdToo, wrote, readed;
-	char buff[1024];
 
-	if (ac != 3)
+/**
+ * closeandfree - Closes file descriptors and frees buffer
+ * @fd1: First file descriptor
+ * @fd2: Second file descriptor
+ * @buff: Buffer to free
+ */
+
+void closeandfree(int fd1, int fd2, char *buff)
+{
+	close(fd1);
+	close(fd2);
+	free(buff);
+}
+
+/**
+ * copy_file - copies a file.
+ * @file_from: File from copy
+ * @file_to: File to copy.
+ * Return: 1 on success, -1 on fail
+ */
+
+int copy_file(const char *file_from, const char *file_to)
+{
+	int fd1, fd2, lenfrom, lento, aux;
+	char *buff;
+
+	lenfrom = 1;
+	fd1 = open(file_from, O_RDONLY);
+	if (fd1 == -1)
+		return (-1);
+	fd2 = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd2 == -1)
+		return (-2);
+	buff = (char *)malloc(sizeof(char) * 1024);
+	if (buff == NULL)
+		return (0);
+	while (lenfrom > 0)
+	{
+		lenfrom = read(fd1, buff, 1024);
+		if (lenfrom == -1)
+		{
+			closeandfree(fd1, fd2, buff);
+			return (-1);
+		}
+		lento = write(fd2, buff, lenfrom);
+		if (lento == -1)
+		{
+			closeandfree(fd1, fd2, buff);
+			return (-2);
+		}
+	}
+	aux = close(fd1);
+	if (aux == -1)
+		return (fd1);
+	aux = close(fd2);
+	if (aux == -1)
+		return (fd2);
+	free(buff);
+	return (1);
+}
+
+/**
+ * main - Main function for cp
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: Always 0
+ */
+
+int main(int argc, char **argv)
+{
+	int aux;
+
+	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	fdFrum = open(av[1], O_RDONLY);
-	if (fdFrum == -1)
+	aux = copy_file(argv[1], argv[2]);
+	if (aux == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	fdToo = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (fdToo == -1)
+	else if (aux == -2)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-	while ((readed = read(fdFrum, buff, 1024)) > 0)
+	else if (aux != 1)
 	{
-		wrote = write(fdToo, buff, readed);
-		if (wrote == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-			exit(99);
-		}
-	}
-	if (readed == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
-	if (close(fdFrum) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d", fdFrum);
-		exit(100);
-	}
-	if (close(fdToo) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d", fdToo);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", aux);
 		exit(100);
 	}
 	return (0);
